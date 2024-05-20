205646fdabca4e3304af7ac8920aca6054ee43a8
MDtheProgrammer <dhinukutty33@gmail.com>
2024-05-21 00:49:48 +0530

    Update CafeManagementSystemApplication.java

--- cafe.inn.cafe/src/main/java/com/inn/cafe/CafeManagementSystemApplication.java
+++ cafe.inn.cafe/src/main/java/com/inn/cafe/CafeManagementSystemApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CafeManagementSystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello there!");
 		SpringApplication.run(CafeManagementSystemApplication.class, args);
+		
 	}
 
 }
