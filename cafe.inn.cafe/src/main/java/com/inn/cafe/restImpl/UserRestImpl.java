e7ebfff5df2b5a5645b945641a04edcd18556fc8
MDtheProgrammer <dhinukutty33@gmail.com>
2024-05-21 14:07:54 +0530

    created something went wrong message

--- cafe.inn.cafe/src/main/java/com/inn/cafe/restImpl/UserRestImpl.java
+++ cafe.inn.cafe/src/main/java/com/inn/cafe/restImpl/UserRestImpl.java
@@ -2,21 +2,30 @@ package com.inn.cafe.restImpl;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.inn.cafe.constants.CafeConstants;
 import com.inn.cafe.rest.UserRest;
+import com.inn.cafe.service.UserService;
+import com.inn.cafe.utils.CafeUtils;
 
 @RestController
 public class UserRestImpl implements UserRest{
 
     @Override
     public ResponseEntity<String> signup(Map<String, String> requestMap) {
-       try{
 
+        UserService userService;
+
+       try{
+        userService.signUp(requestMap);
        }catch(Exception ex){
         ex.printStackTrace();
        }
+
+       return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
