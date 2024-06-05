4c1e46ca30f70568c32937aea66b406cf9eec534
MDtheProgrammer <dhinukutty33@gmail.com>
2024-06-05 18:06:32 +0530

    category started

---
+++ cafe.inn.cafe/src/main/java/com/inn/cafe/POJO/Category.java
@@ -0,0 +1,36 @@
+package com.inn.cafe.POJO;
+
+import java.io.Serializable;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@NamedQuery(name="Category.getAllCategory", query="select c from category c")
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "category")
+public class Category implements Serializable{
+
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+}
---
+++ cafe.inn.cafe/src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -0,0 +1,7 @@
+package com.inn.cafe.serviceImpl;
+
+import com.inn.cafe.service.CategoryService;
+
+public class CategoryServiceImpl implements CategoryService{
+
+}
